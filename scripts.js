8c3f7ed4960736c58b75152f3b2f5e08238e37d8
jjfowler7 <jjfowler7@gmail.com>
2022-01-29 10:33:58 -0500

    Add input box and button to accept number and dynamically update grid

--- scripts.js
+++ scripts.js
@@ -1,13 +1,37 @@
 const container = document.querySelector('#container');
 
-function generateDivs(number = 16) {
-    for(let i = 0; i < number * number; i++) {
-        const divs = document.createElement('div');
-        divs.textContent = `div # ${i}`;
-        container.append(divs);
-    }
-    let gridTemplateColumns = `repeat(${number}, 1fr)`;
+
+
+function generateDivs() {
+    let number = document.getElementById('gridSize').value; //get input from text box 
+    let divCount = container.childElementCount; // count the number of divs and store count in divCount
+
+    
+    if (number * number > divCount) { // if input number higher than divCount then add differnece
+        numberAdd = (number * number) - divCount;
+        for (let i = 0; i < numberAdd; i++) {
+            const divs = document.createElement('div');
+            container.append(divs);
+        }
+    } else if (number * number < divCount) { // if input number is lower than div count than subtract difference
+        numberSubtract = divCount - (number * number);
+        for (let i = 0; i < numberSubtract; i++) {
+            const divs = document.createElement('div');
+            container.removeChild(container.firstChild);
+        }
+    } 
+
+    gridTemplateColumns = `repeat(${number}, 1fr)`;
     document.getElementById('container').style.gridTemplateColumns = gridTemplateColumns;
+
 }
+
+
+
+
+
+
+
+
 
 generateDivs();
