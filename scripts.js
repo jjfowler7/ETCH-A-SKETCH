d8db9f3f5500e675e9e0b982581f9493a11a1af9
jjfowler7 <jjfowler7@gmail.com>
2022-01-29 10:59:35 -0500

    Prohibit user from entering number larger than 100

--- scripts.js
+++ scripts.js
@@ -3,12 +3,14 @@ const container = document.querySelector('#container');
 function generateDivs() {
     let number = document.getElementById('gridSize').value; //get input from text box 
     let divCount = container.childElementCount; // count the number of divs and store count in divCount
-    
-    if(divCount === 0){ //default grid to 16 on load
+
+    if (divCount === 0) { //default grid to 16 on load
         number = 16;
-    } 
-    
-    if (number * number > divCount) { // if input number higher than divCount then add differnece
+    }
+
+    if (number > 100) {
+        return false;
+    } else if (number * number > divCount) { // if input number higher than divCount then add differnece
         numberAdd = (number * number) - divCount;
         for (let i = 0; i < numberAdd; i++) {
             const divs = document.createElement('div');
@@ -20,7 +22,7 @@ function generateDivs() {
             const divs = document.createElement('div');
             container.removeChild(container.firstChild);
         }
-    } 
+    }
 
     gridTemplateColumns = `repeat(${number}, 1fr)`;
     document.getElementById('container').style.gridTemplateColumns = gridTemplateColumns;
